9f78a4fd2491a9d362759f1d83b7f69643f70916
Simon S <simschn20@gmail.com>
2022-11-09 22:16:14 +0100

    add moving platforms

---
+++ scenes/platform_set1.gd
@@ -0,0 +1,24 @@
+extends Node2D
+var time = 0
+var move_range = 300
+var platforms
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	platforms = get_children()
+	platforms.sort_custom(sort_by_position)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta * 2
+	var counter = PI/16
+	for platform in platforms:
+		counter += PI/16
+	#	platform.position.x = tan(time * PI / 2) * 300
+		platform.position.x = sin(time + counter) * 400
+		print(time + counter)
+
+
+func sort_by_position(a, b):
+	if a.position.y < b.position.y:
+		return true
+	return false
--- scripts/platform_generator.gd
+++ scripts/platform_generator.gd
@@ -2,27 +2,31 @@ extends Node2D
 
 @export var player:Node2D
 
-var platform_set1
 var height = 1600;
 var lowest_y = 0
+var sets = []
 var active_sets = []
 var repeat_texture:Texture ;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	platform_set1 = load("res://scenes/platform_set1.tscn")
-	repeat_texture= load("res://assets/images/repeat.png")
-	var new_set = platform_set1.instantiate()
+	repeat_texture = load("res://assets/images/repeat.png")
+	sets.append(load("res://scenes/platform_set1.tscn"))
+	sets.append(load("res://scenes/platform_set2.tscn"))
+	var new_set = sets[0].instantiate()
 	active_sets.append(new_set)
 	add_child(new_set)
-	var child:Camera2D = player.get_child(2)
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var respawn = Input.is_key_pressed(KEY_R)
+	if respawn:
+		player.position.y = lowest_y
 	if player.position.y < lowest_y:
 		lowest_y -= height
-		var new_set = platform_set1.instantiate()
+		#var new_set = sets[-lowest_y / height % 2].instantiate()
+		var new_set = sets[0].instantiate()
 		active_sets.append(new_set)
 		add_child(new_set)
 		new_set.position.y = lowest_y
